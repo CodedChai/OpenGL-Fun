28a97bfd981a897be83887d744c14fbca37ada09
Skrillcon <guardianx515@gmail.com>
2017-11-09 14:27:01 -0600

    Setup GLEW

--- OpenGLFun/OpenGLFun/src/Application.cpp
+++ OpenGLFun/OpenGLFun/src/Application.cpp
@@ -1,4 +1,6 @@
-#include  <glfw3.h>
+#include <GL\glew.h>
+#include  <GLFW\glfw3.h>
+#include <iostream>
 
 int main(void)
 {
@@ -9,7 +11,7 @@ int main(void)
 		return -1;
 
 	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	window = glfwCreateWindow(1280, 720, "Woohoo!", NULL, NULL);
 	if (!window)
 	{
 		glfwTerminate();
@@ -19,17 +21,34 @@ int main(void)
 	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
 
+	if (glewInit() != GLEW_OK)
+		return -1;
+
+	std::cout << glGetString(GL_VERSION) << std::endl;
+
+	// X, Y coordinates
+	float positions[6] = {
+		-0.5f, -0.5f,
+		 0.0f,  0.5f,
+		 0.5f, -0.5f
+	};
+
+	unsigned int buffer;
+	glGenBuffers(1, &buffer);
+	glBindBuffer(GL_ARRAY_BUFFER, buffer);
+	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2*sizeof(float), 0);
+
+
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
 	{
 		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		glBegin(GL_TRIANGLES);
-		glVertex2f(-0.5f, -0.5f);
-		glVertex2f(0.0f, 0.5f);
-		glVertex2f(0.5f, -0.5f);
-		glEnd();
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
