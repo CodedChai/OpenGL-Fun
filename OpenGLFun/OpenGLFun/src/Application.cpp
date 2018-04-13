89ccb053c102d02449086b97a9c6e335d7576d11
Skrillcon <guardianx515@gmail.com>
2018-04-12 21:52:01 -0500

    Set up texture loading using stb_image

--- OpenGLFun/OpenGLFun/src/Application.cpp
+++ OpenGLFun/OpenGLFun/src/Application.cpp
@@ -7,6 +7,7 @@
 #include "VertexArray.h"
 #include "VertexBufferLayout.h"
 #include "Shader.h"
+#include "Texture.h"
 
 
 int main(void)
@@ -43,10 +44,10 @@ int main(void)
 		// X, Y coordinates
 		// Remember it's counter clockwise
 		float positions[] = {
-			-0.5f, -0.5f,	// 0
-			 0.5f, -0.5f,	// 1
-			 0.5f,  0.5f,	// 2
-			-0.5f,  0.5f	// 3
+			-0.5f, -0.5f, 0.0f, 0.0f,	// 0
+			 0.5f, -0.5f, 1.0f, 0.0f,	// 1
+			 0.5f,  0.5f, 1.0f, 1.0f,	// 2
+			-0.5f,  0.5f, 0.0f, 1.0f	// 3
 		};
 
 		unsigned int indices[] = {
@@ -54,10 +55,14 @@ int main(void)
 			2, 3, 0
 		};
 
+		GLCall(glEnable(GL_BLEND));
+		GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
+
 		VertexArray va;
-		VertexBuffer vb(positions, 4 * 2 * sizeof(float));
+		VertexBuffer vb(positions, 4 * 4 * sizeof(float));
 
 		VertexBufferLayout layout;
+		layout.Push<float>(2);
 		layout.Push<float>(2);
 		va.AddBuffer(vb, layout);
 
@@ -66,6 +71,10 @@ int main(void)
 		Shader shader("res/shaders/Basic.shader");
 		shader.Bind();
 		shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
+
+		Texture texture("res/textures/marble.png");
+		texture.Bind();
+		shader.SetUniform1i("u_Texture", 0);
 
 		va.Unbind();
 		shader.Unbind();
--- OpenGLFun/OpenGLFun/src/Shader.h
+++ OpenGLFun/OpenGLFun/src/Shader.h
@@ -24,6 +24,9 @@ public:
 
 	// Set uniforms
 	void SetUniform4f(const std::string name, float v0, float v1, float v2, float v3);
+	void SetUniform1f(const std::string name, float value);
+	void SetUniform1i(const std::string name, int value);
+
 private:
 	ShaderProgramSource ParseShader(const std::string& filepath);
 	unsigned int CompileShader(unsigned int type, const std::string& filepath);
