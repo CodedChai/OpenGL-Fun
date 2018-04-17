147e5ce1fe6772481e68ebaff91bfa466d0a743d
Skrillcon <guardianx515@gmail.com>
2018-04-16 22:42:21 -0500

    Add some more MVP features

--- OpenGLFun/OpenGLFun/src/Application.cpp
+++ OpenGLFun/OpenGLFun/src/Application.cpp
@@ -45,10 +45,10 @@ int main(void)
 		// X, Y coordinates
 		// Remember it's counter clockwise
 		float positions[] = {
-			-0.5f, -0.5f, 0.0f, 0.0f,	// 0
-			 0.5f, -0.5f, 1.0f, 0.0f,	// 1
-			 0.5f,  0.5f, 1.0f, 1.0f,	// 2
-			-0.5f,  0.5f, 0.0f, 1.0f	// 3
+			100.f, 100.f, 0.0f, 0.0f,	// 0
+			200.f, 100.f, 1.0f, 0.0f,	// 1
+			200.f, 200.f, 1.0f, 1.0f,	// 2
+			100.f, 200.f, 0.0f, 1.0f	// 3
 		};
 
 		unsigned int indices[] = {
@@ -69,12 +69,16 @@ int main(void)
 
 		IndexBuffer ib(indices, 6);
 
-		glm::mat4 proj = glm::ortho(-2.0f, 2.0f, -1.25f, 1.25f, -1.0f, 1.0f);
+		glm::mat4 proj = glm::ortho(0.0f, 1280.f, 0.f, 720.f, -1.0f, 1.0f);
+		glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-100, -100, 0));
+		glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(200., 200., 0));
+
+		glm::mat4 mvp = proj * view * model;
 
 		Shader shader("res/shaders/Basic.shader");
 		shader.Bind();
 		shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
-		shader.SetUniformMat4f("u_MVP", proj);
+		shader.SetUniformMat4f("u_MVP", mvp);
 
 		Texture texture("res/textures/transparent.png");
 		texture.Bind();
